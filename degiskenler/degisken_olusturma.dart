646e5b0dad946b575cdb435cc43b210d1a456963
yasinkrc <113844850+yasinkrc@users.noreply.github.com>
2025-01-01 23:45:04 +0300

    Add files via upload

---
+++ degiskenler/degisken_olusturma.dart
@@ -0,0 +1,39 @@
+
+
+  void main(){
+
+    var ogrenci_Adi="Ahmet";
+    var ogrenciYas=23;
+    var ogrenciBoy=1.79;
+    var ogrenciBasharf='A' ; // "A"
+    var ogrenciDevamEdiyorMu=true;
+
+
+    print(ogrenci_Adi);
+    print(ogrenciYas);
+    print(ogrenciBoy);
+    print(ogrenciBasharf);
+    print(ogrenciDevamEdiyorMu);
+
+    int urun_id=3416 ;
+    String urun_adi= "Kol saati";
+    int urun_adet=100;
+    double urun_fiyat=149.99;
+    String urun_tedarikci= "Rolex";
+
+
+    print("Ürün idi : $urun_id");
+    print("Ürün Adi : $urun_adi");
+    print("Ürün adet: $urun_adet");
+    print("Ürün Fiyat:$urun_fiyat ₺");
+    print("Ürün tedarikçi $urun_tedarikci");
+
+    int a =100;
+    int b =200;
+
+    print("$a ve $b nin toplamı : ${a+b}");
+    print("$a ve $b nin çarpımı : ${a*b}");
+    print("$a ve $b nin bölümü : ${a/b}");
+    print("$a ve $b nin farkı : ${a-b}");
+
+  }
---
+++ degiskenler/sabitler.dart
@@ -0,0 +1,29 @@
+void main(){
+
+  // sabitler
+  // final , const , java (final ) , Swift (let) , kotlin ( val  )
+
+  int sayi = 30 ;
+  print(sayi);
+  sayi=100;
+  print(sayi);
+
+  // final
+
+  final int numara = 100;
+  print(numara);
+
+  // numara=4; değer atamazsın !
+
+  const int sonuc = 500;
+  print(sonuc);
+  //sonuc=400; değer atayamazsın .
+
+  // arayüzde const kullanılır .
+
+  // kodlamayo çalıştırdığımızda  hafızada oluşur --final
+  // kodlamayo çalıştırmadığımızda hafızada oluşur -- const genelde resimler falan
+
+
+
+}
